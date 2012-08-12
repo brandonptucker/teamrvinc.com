d1c6e7abb633151618bdf2609479183f9f28ee04
brandon <btucker84@gmail.com>
2012-08-12 16:07:54 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamRV::Application.config.secret_token = '32287b0f8c6bffeca4ba85a58b247dcde1bbde77b7a4a70bb3f858562a1e8d9214144a8bbfe2d664532ecd75d9c925c3a69128e7f7a7c42683ad79f6b94e3af2'
