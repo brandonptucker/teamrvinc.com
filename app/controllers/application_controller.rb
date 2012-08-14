fb690463fe6ea78677da1586ec64a617c8fa8a4f
brandon <btucker84@gmail.com>
2012-08-13 21:43:23 -0400

    Added sessions controller and user model (login and logout)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def authorize
+    unless signed_in?
+      redirect_to signin_path
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :logged_in?, :current_user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ TeamRV::Application.routes.draw do
   
   root :to => "pages#home"
   match "contact" => "pages#contact"
+
+  match "/login",  to: "sessions#new"
+  match "/logout", to: "sessions#destroy", via: :delete
+
+  resources :sessions, only: [:new, :create, :destroy]
 end
