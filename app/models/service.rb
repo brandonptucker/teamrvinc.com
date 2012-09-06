6affc64296370fad13e8638036d859351657e00c
brandon <btucker84@gmail.com>
2012-09-05 21:10:56 -0400

    Display preview of current image for edit service

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,6 @@
 class Service < ActiveRecord::Base
   attr_accessible :active, :name, :price, :image
-  has_attached_file :image, :styles => { :medium => "300x225>" }
+  has_attached_file :image, :styles => { :small => "100x75>", :medium => "300x225>", :large => "600x450>" }
   validates :name, :price, :presence => true
   validates :price, :numericality => true
   validates :image, :attachment_presence => true
