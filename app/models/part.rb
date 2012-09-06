c1a52025a3aad9660de1503f809468b95cbe0806
brandon <btucker84@gmail.com>
2012-09-05 20:50:52 -0400

    Display preview of current image for edit part

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,6 +1,6 @@
 class Part < ActiveRecord::Base
   attr_accessible :description, :list_price, :name, :part_number, :price, :image
-  has_attached_file :image, :styles => { :medium => "300x225>", :large => "600x450>" }
+  has_attached_file :image, :styles => { :small => "100x75>", :medium => "300x225>", :large => "600x450>" }
   validates :description, :list_price, :name, :part_number, :price, :presence => true
   validates :list_price, :price, :numericality => true
   validates :image, :attachment_presence => true
