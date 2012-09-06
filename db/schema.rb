c36f224d1c544355f3587edb763db5c48e468a8c
brandon <btucker84@gmail.com>
2012-09-05 21:23:38 -0400

    Added description to services

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120906005242) do
+ActiveRecord::Schema.define(:version => 20120906011915) do
 
   create_table "assets", :force => true do |t|
     t.string   "asset_file_name"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(:version => 20120906005242) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.text     "description"
   end
 
   create_table "users", :force => true do |t|
