0d55b6c208c8cd0aa418a5764c6d38c6828fb47f
Brandon Tucker <btucker84@gmail.com>
2013-12-16 00:01:28 -0500

    Destroy original file and directory

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -2,4 +2,13 @@ class Asset < ActiveRecord::Base
   attr_accessible :asset
   belongs_to :rv
   has_attached_file :asset, :styles => { :small => "100x75>", :medium => "300x225>", :large => "600x450>" }
+  after_save :destroy_original_directory
+
+	private
+	def destroy_original_directory
+		original_directory = File.dirname(self.asset.path)
+		if Dir.exists?(original_directory)
+			FileUtils.rm_rf(original_directory)
+		end
+	end
 end
