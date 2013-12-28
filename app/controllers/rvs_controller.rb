46a87ce1b8a2871ab76a50c2f8cdb04c2db508f4
Brandon Tucker <btucker84@gmail.com>
2013-12-28 20:16:14 +0000

    Changed the title of the rvs page

--- app/controllers/rvs_controller.rb
+++ app/controllers/rvs_controller.rb
@@ -7,7 +7,6 @@ class RVsController < ApplicationController
                         .group_by(&:category)
                         .sort { |a,b| b[1].length <=> a[1].length }
     @categories = RV.uniq.pluck(:category)
-    @makes = RV.uniq.pluck(:make)
 
     respond_to do |format|
       format.html # index.html.erb
