816c6d27b20d01e135f17b6bfd274db932be33ac
Brandon Tucker <btucker84@gmail.com>
2014-01-10 01:57:04 +0000

    Added thumbnail to processors to fix problem with image upload not resizing

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,8 +1,9 @@
 class Asset < ActiveRecord::Base
   attr_accessible :asset
   belongs_to :rv
-  has_attached_file :asset, :styles => { :small => "100x75>", :medium => "300x225>", :large => "600x450>" }, 
-  	:processors => [:compression]
+  has_attached_file :asset, 
+  									:styles => { :small => "100x75>", :medium => "300x225>", :large => "600x450>" }, 
+  									:processors => [:thumbnail, :compression]
   after_save :destroy_original_directory
 
 	private
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,7 +1,8 @@
 class Service < ActiveRecord::Base
   attr_accessible :active, :name, :price, :image, :description
-  has_attached_file :image, :styles => { :small => "100x75>", :medium => "300x225>", :large => "600x450>" }, 
-  	:processors => [:compression]
+  has_attached_file :image, 
+  									:styles => { :small => "100x75>", :medium => "300x225>", :large => "600x450>" }, 
+  									:processors => [:thumbnail, :compression]
   validates :description, :name, :price, :presence => true
   validates :price, :numericality => true
   validates :image, :attachment_presence => true
